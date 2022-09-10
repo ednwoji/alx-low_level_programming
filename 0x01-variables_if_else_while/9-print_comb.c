8d06cbac973360e822ddf941c262770db6ffe2ae
ednwoji <davidnwoji@gmail.com>
2022-09-09 21:52:57 -0700

    Modified 10th file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,13 @@ int main(void)
 	for (num = 0; num < 9; num++)
 	{
 		putchar (num + '0');
-		putchar (',');
-		putchar (' ');
+		
+		if (num != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
 	}
-	putchar ("9\n");
+	putchar ("\n");
 	return (0);
 }
