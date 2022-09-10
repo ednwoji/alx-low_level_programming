6839b48f3260f7b7a57b1ca2a1c0423d564783af
ednwoji <davidnwoji@gmail.com>
2022-09-09 21:47:19 -0700

    Modified 10th task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 9; num++)
 	{
 		putchar (num + '0');
 		putchar (',');
 		putchar (' ');
 	}
+	putchar ('9');
 	putchar ('\n');
 	return (0);
 }
