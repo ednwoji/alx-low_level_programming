b7995680eb613c8f1fe5218c7c4a4a910bf8890c
ednwoji <davidnwoji@gmail.com>
2022-09-13 17:33:21 -0700

    Adding task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * print alphabet - Check description
+ * Description: It prints the alphabet in lowecase followed by a new line
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
