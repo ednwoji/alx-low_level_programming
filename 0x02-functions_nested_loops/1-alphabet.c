1b76e09948ece39705f2d76221cf6a2657108427
ednwoji <davidnwoji@gmail.com>
2022-09-13 17:36:05 -0700

    COrrection to task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 
 /**
- * print alphabet - Check description
- * Description: It prints the alphabet in lowecase followed by a new line
- * Return: Nothing
+ * main - Check description
+ * 
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
