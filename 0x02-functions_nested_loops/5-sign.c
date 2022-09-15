a6c3925829f03af12214efbf7d0d52a4d07e65f3
ednwoji <davidnwoji@gmail.com>
2022-09-14 18:05:48 -0700

    Adding 6th file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * int print_sign(int n) - Check the description
+ * Description: Prints the sign on a number
+ * Return: 1 if greater than zero, 0 if equal to zero and -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1)
+	}
+}
