f31cdbbd75c1b469eeda0413be6f96a6052526d3
ednwoji <davidnwoji@gmail.com>
2022-09-14 18:57:36 -0700

    Added 9th task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * void jack_bauer(void) - Check description
+ * Description: Prints every minute of the day starting from 00:00
+ * Return: 0 if successful
+ */
+void jack_bauer(void)
+{
+	for (t = 00:00; t <= 23:59; t++)
+	{
+		_putchar (t + '0');
+		_putchar ('\n')';
+	}
+}
