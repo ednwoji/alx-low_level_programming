cde6c08ad4ad856703aa97f97575c15d482a61f6
ednwoji <davidnwoji@gmail.com>
2022-09-14 19:18:25 -0700

    Adding 11th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * int add(int a, int b) - Check description below
+ * Description: For addition of two integers
+ * Return: Result if Successful
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
