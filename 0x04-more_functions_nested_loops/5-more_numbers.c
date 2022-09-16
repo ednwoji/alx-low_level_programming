dbb6b14d9a47efa682b308a9c889e746c9932712
ednwoji <davidnwoji@gmail.com>
2022-09-15 18:04:36 -0700

    Added 6th task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void more_numbers - print 10 times the numbers from 0 t0 14
+ *
+ */
+void more_numbers(void)
+{
+	int num;
+	int space;
+
+	for (space = 1; space <= 10; space++)
+	{
+		for (num = 0; num <=14; num++)
+		{
+			_putchar (num + '0');
+		}
+		_putchar ('\n');
+	}
+	_putchar ('\n');
+}
