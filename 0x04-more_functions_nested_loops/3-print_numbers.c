4930e577e0bb6b26cc609c84d4236b87defe683e
ednwoji <davidnwoji@gmail.com>
2022-09-15 17:38:18 -0700

    Modify

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
  */
 void print_numbers(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar (num);
-		_putchar ('\n');
+		_putchar (num + '0');
 	}
-	return (0);
+	_putchar('\n');
 }
