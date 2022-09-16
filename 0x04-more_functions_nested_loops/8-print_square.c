c842f1bb0d0bfd233fae3502375df2fa95e14c1a
ednwoji <davidnwoji@gmail.com>
2022-09-15 19:08:21 -0700

    Add 9th file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: Input integer
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar ('#')
+				for (j = 0; j <= i; j++)
+				{
+					_putchar ('\n')
+				}
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
