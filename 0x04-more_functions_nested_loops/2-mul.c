bb52581e22e447779cfac517c483e90078c258bd
ednwoji <davidnwoji@gmail.com>
2022-09-15 17:26:04 -0700

    Adding 3rd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * int mul(int a, int b) - Check description
+ * Description: Multiplies two integers
+ * @a: Input character
+ * @b: input character
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+	
+	return (product);
+}
