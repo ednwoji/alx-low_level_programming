59fd4458221d69a6332b14d06f98e7504f387dee
ednwoji <davidnwoji@gmail.com>
2022-09-15 17:08:33 -0700

    Modify 1st task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 
 /**
  * int _isupper(int c) - Check the description
+ * @c: An input character
  * Description - Checks for uppercase character
  * Return: 1 if true, 0 if false
  */
 int _isupper(int c)
 {
 	char alpha = 'A';
+	int isupper = 0;
+
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 	{
 		if (c == alpha)
 		{
-			return (1);
-		}
-		else
-		{
-			return (0);
+			isupper = 1;
 		}
 	}
+	return (isupper);
 }
