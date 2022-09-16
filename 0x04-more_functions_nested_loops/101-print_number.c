d9298968bee64cabddae75c0587b79207407c678
ednwoji <davidnwoji@gmail.com>
2022-09-15 20:16:38 -0700

    Additional tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: An input integer.
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	_putchar (n + '0');
+}
