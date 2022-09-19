87402543b9c3dd9ec541ca83353a33c30754e25e
ednwoji <davidnwoji@gmail.com>
2022-09-19 16:43:51 -0700

    Adding array and string

---
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int as parameter and updates 
+ * The value it points to to 98
+ * @*n: integer input
+ * Return: Always 0 Successful
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
