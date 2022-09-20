c1277ea52e238434c6a199c0b16127454b82f6c6
ednwoji <davidnwoji@gmail.com>
2022-09-19 17:32:01 -0700

    Adding 4th task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line
+ * @str: Input character
+ * Return: NOthing.
+ */
+void _puts(char *str)
+{
+	_putchar (*str);
+	_putchar ('\n');
+}
