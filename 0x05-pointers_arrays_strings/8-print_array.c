57432703551497a3d5db3119308e943125fb7abf
ednwoji <davidnwoji@gmail.com>
2022-09-19 23:02:35 -0700

    Adding 9th task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: Input array
+ * @n: Input integer
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar (a[i]);
+		_putchar ('\n');
+	}	
+}
