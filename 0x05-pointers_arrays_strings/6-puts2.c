3be9daf78b7200195b4b56ea2714b8798be4af63
ednwoji <davidnwoji@gmail.com>
2022-09-19 22:41:37 -0700

    Modify

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,9 +12,9 @@ void puts2(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	len -=1;
+	len -= 1;
 
-	for (; i <= len; i +=2)
+	for (; i <= len; i += 2)
 		_putchar (str[i]);
 
 	_putchar ('\n');
