5f372a87b14964580853cf19c5fbcb5ba066ad79
ednwoji <davidnwoji@gmail.com>
2022-10-03 18:17:09 -0700

    Adding 3rd task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints the result of the multiplication
+ * Followed by a new line
+ * @argc: Argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, z = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			z *= argv[i];
+		}
+		printf("%d\n", z);
+	}
+	else
+		printf("Error");
+	return (0);
+}
