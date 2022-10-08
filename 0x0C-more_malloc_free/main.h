230416eea85cc748cb375e695aa46b625aed0041
ednwoji <davidnwoji@gmail.com>
2022-10-07 22:54:43 -0700

    Modify

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -57,3 +57,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char  *s2, unsigned int n);
